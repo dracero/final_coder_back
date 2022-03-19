83fa159e7251a28586a43b007fcae756cdf7a147
Marcelo Beretta <mfberetta@gmail.com>
2022-03-19 17:06:48 -0300

    entrega desafio

--- config/mailer.js
+++ config/mailer.js
@@ -4,8 +4,8 @@ const transporter = createTransport({
     service: 'gmail',
     port: 587,
     auth: {
-        user: 'notificacion.ch23105@gmail.com',
-        pass: 'c4e7db1b'
+        user: 'xxxxx@gmail.com',
+        pass: 'xxxxx'
     }
 })
 export default transporter
