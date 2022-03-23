d518d831b40a4db0776604f31b4bf34bb5bdd9d1
dracero <diego.racero@gmail.com>
2022-03-22 22:36:51 -0300

    primero

--- config/config.js
+++ config/config.js
@@ -3,17 +3,14 @@ dotenv.config()
 
 let administrador
 
-const mongo_user = process.env.MONGO_USER
-const mongo_password = process.env.MONGO_PASSWORD
-const mongo_server = process.env.MONGO_SERVER
-const mongo_base = process.env.MONGO_BASE
+
 
 export default {
     fileSystem: {
         path: './DB'
     },
     mongodb: {
-        cnxStr: 'mongodb+srv://' + mongo_user + ':' + mongo_password + '@' + mongo_server + '/' + mongo_base,
+        cnxStr: process.env.MONGO_URL,
         options: {
             useNewUrlParser: true,
             useUnifiedTopology: true,
@@ -21,8 +18,8 @@ export default {
         }
     },
     admin: {
-        username: 'admin@e.commerse',
-        password: '4dm1n'
+        username: 'admin@final',
+        password: 'admin'
     },
     setUserRol: (rol) => {
         if(rol == 'admin') administrador = true
